1c72e4aa298d6441e89976282fa7e4e84a98e5c0
Admin <nguyenvantuyen6789@gmail.com>
2021-08-31 21:02:32 +0700

    Gom các component vào 1 folder "Component"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
-import { BodyComponent } from './body/body.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { BodyComponent } from './component/body/body.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
 
 @NgModule({
   declarations: [
